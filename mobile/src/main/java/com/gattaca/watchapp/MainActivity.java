f16901d8465914febd40e15f60b64265fda47e44
holoed <Chicago%90>
2014-08-18 00:15:47 +0100

    Added speech support to android wear

--- mobile/src/main/java/com/gattaca/watchapp/MainActivity.java
+++ mobile/src/main/java/com/gattaca/watchapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.media.MediaRouteSelector;
@@ -17,17 +19,31 @@ import com.google.android.gms.cast.Cast;
 import com.google.android.gms.cast.CastDevice;
 import com.google.android.gms.cast.CastMediaControlIntent;
 import com.google.android.gms.cast.MediaInfo;
+import com.google.android.gms.cast.MediaMetadata;
 import com.google.android.gms.cast.RemoteMediaPlayer;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.images.WebImage;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
 public class MainActivity extends Activity {
 
+    private HashMap<String, MediaInfo> movies;
     private MediaRouter mMediaRouter;
     private MediaRouteSelector mMediaRouteSelector;
     private String APPLICATION_ID;
@@ -42,6 +58,14 @@ public class MainActivity extends Activity {
         public void onReceive(Context context, Intent intent) {
             // intent can contain anydata
             Log.d("sohail", "onReceive called");
+
+            byte[] data = intent.getByteArrayExtra("message");
+            String spokenText = new String(data);
+
+            if (!movies.containsKey(spokenText))
+                return;
+
+            final MediaInfo selectedMovie = movies.get(spokenText);
 
             CastDevice device = CastDevice.getFromBundle(routes.get(0).getExtras());
 
@@ -58,12 +82,12 @@ public class MainActivity extends Activity {
                                         public void onResult(Cast.ApplicationConnectionResult applicationConnectionResult) {
                                             mRemoteMediaPlayer = new RemoteMediaPlayer();
 
-                                            MediaInfo mediaInfo = new MediaInfo.Builder("http://192.168.0.8/Movies/Memento.mp4")
-                                                    .setContentType("video/mp4")
-                                                    .setStreamType(MediaInfo.STREAM_TYPE_BUFFERED)
-                                                    .build();
+//                                            MediaInfo mediaInfo = new MediaInfo.Builder("http://192.168.0.8/Movies/Memento.mp4")
+//                                                    .setContentType("video/mp4")
+//                                                    .setStreamType(MediaInfo.STREAM_TYPE_BUFFERED)
+//                                                    .build();
 
-                                            mRemoteMediaPlayer.load(mApiClient, mediaInfo, true)
+                                            mRemoteMediaPlayer.load(mApiClient, selectedMovie, true)
                                                     .setResultCallback(new ResultCallback<RemoteMediaPlayer.MediaChannelResult>() {
                                                         @Override
                                                         public void onResult(RemoteMediaPlayer.MediaChannelResult mediaChannelResult) {
@@ -104,6 +128,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AsyncTask<Void, Void, HashMap<String, MediaInfo>>(){
+            @Override
+            protected HashMap<String, MediaInfo> doInBackground(Void... voids) {
+                return buildMedia("http://192.168.0.8:8000/Catalog");
+            }
+
+            @Override
+            protected void onPostExecute(HashMap<String, MediaInfo> mediaInfos) {
+                super.onPostExecute(mediaInfos);
+                movies = mediaInfos;
+            }
+        }.execute();
+
         DiscoverCastDevices();
     }
 
@@ -150,5 +188,98 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private JSONObject parseUrl(String urlString) {
+        InputStream is = null;
+        try {
+            java.net.URL url = new java.net.URL(urlString);
+            URLConnection urlConnection = url.openConnection();
+            is = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    urlConnection.getInputStream(), "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            String json = sb.toString();
+            return new JSONObject(json);
+        } catch (Exception e) {
+            Log.d("", "Failed to parse the json for media list", e);
+            return null;
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+        }
+    }
+
+    private static String TAG_CATEGORIES = "categories";
+    private static String TAG_NAME = "name";
+    private static String TAG_STUDIO = "studio";
+    private static String TAG_SOURCES = "sources";
+    private static String TAG_SUBTITLE = "subtitle";
+    private static String TAG_THUMB = "image-480x270"; // "thumb";
+    private static String TAG_IMG_780_1200 = "image-780x1200";
+    private static String TAG_TITLE = "title";
+    private static String TAG_MEDIA = "videos";
+
+    private HashMap<String, MediaInfo> buildMedia(String url) {
+
+        try {
+            HashMap<String, MediaInfo> mediaList = new HashMap<String, MediaInfo>();
+            JSONObject jsonObj = parseUrl(url);
+            JSONArray categories = jsonObj.getJSONArray(TAG_CATEGORIES);
+            if (null != categories) {
+                for (int i = 0; i < categories.length(); i++) {
+                    JSONObject category = categories.getJSONObject(i);
+                    category.getString(TAG_NAME);
+                    JSONArray videos = category.getJSONArray(TAG_MEDIA);
+                    if (null != videos) {
+                        for (int j = 0; j < videos.length(); j++) {
+                            JSONObject video = videos.getJSONObject(j);
+                            String subTitle = video.getString(TAG_SUBTITLE);
+                            JSONArray videoUrls = video.getJSONArray(TAG_SOURCES);
+                            if (null == videoUrls || videoUrls.length() == 0) {
+                                continue;
+                            }
+                            String videoUrl = videoUrls.getString(0);
+                            String imageurl = video.getString(TAG_THUMB);
+                            String bigImageurl = video.getString(TAG_IMG_780_1200);
+                            String title = video.getString(TAG_TITLE);
+                            String studio = video.getString(TAG_STUDIO);
+                            mediaList.put(title.toLowerCase(), buildMediaInfo(title, studio, subTitle, videoUrl, imageurl,
+                                    bigImageurl));
+                        }
+                    }
+                }
+            }
+            return mediaList;
+        }
+        catch (Exception ex) {
+            return new HashMap<String, MediaInfo>();
+        }
+    }
+
+    private static MediaInfo buildMediaInfo(String title,
+                                            String subTitle, String studio, String url, String imgUrl, String bigImageUrl) {
+        MediaMetadata movieMetadata = new MediaMetadata(MediaMetadata.MEDIA_TYPE_MOVIE);
+
+        movieMetadata.putString(MediaMetadata.KEY_SUBTITLE, subTitle);
+        movieMetadata.putString(MediaMetadata.KEY_TITLE, title);
+        movieMetadata.putString(MediaMetadata.KEY_STUDIO, studio);
+        movieMetadata.addImage(new WebImage(Uri.parse(imgUrl)));
+        movieMetadata.addImage(new WebImage(Uri.parse(bigImageUrl)));
+
+        return new MediaInfo.Builder(url)
+                .setStreamType(MediaInfo.STREAM_TYPE_BUFFERED)
+                .setContentType("video/mp4")
+                .setMetadata(movieMetadata)
+                .build();
     }
 }
--- wear/src/main/java/com/gattaca/watchapp/MainActivity.java
+++ wear/src/main/java/com/gattaca/watchapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gattaca.watchapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.view.View;
@@ -19,20 +21,21 @@ import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
 import com.google.android.gms.wearable.WearableStatusCodes;
 
+import java.util.List;
+
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getName();
     public static final String PATH = "/start/Foo";
 
-    public MainActivity() {
-    }
+   private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final GoogleApiClient googleApiClient = new GoogleApiClient.Builder(this)
+        googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Wearable.API)
                 .build();
 
@@ -46,47 +49,74 @@ public class MainActivity extends Activity {
                 btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        fireMessage();
-                    }
-                });
-            }
-
-            private void fireMessage() {
-                // Send the RPC
-                PendingResult<NodeApi.GetConnectedNodesResult> nodes = Wearable.NodeApi.getConnectedNodes(googleApiClient);
-                nodes.setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
-                    @Override
-                    public void onResult(NodeApi.GetConnectedNodesResult result) {
-                        for (int i = 0; i < result.getNodes().size(); i++) {
-                            Node node = result.getNodes().get(i);
-                            String nName = node.getDisplayName();
-                            String nId = node.getId();
-                            Log.d(TAG, "Node name and ID: " + nName + " | " + nId);
-
-                            Wearable.MessageApi.addListener(googleApiClient, new MessageApi.MessageListener() {
-                                @Override
-                                public void onMessageReceived(MessageEvent messageEvent) {
-                                    Log.d(TAG, "Message received: " + messageEvent);
-                                }
-                            });
-
-                            PendingResult<MessageApi.SendMessageResult> messageResult = Wearable.MessageApi.sendMessage(googleApiClient, node.getId(),
-                                    PATH, null);
-                            messageResult.setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
-                                @Override
-                                public void onResult(MessageApi.SendMessageResult sendMessageResult) {
-                                    Status status = sendMessageResult.getStatus();
-                                    Log.d(TAG, "Status: " + status.toString());
-                                    if (status.getStatusCode() != WearableStatusCodes.SUCCESS) {
-                                        //alertButton.setProgress(-1);
-                                        //label.setText("Tap to retry. Alert not sent :(");
-                                    }
-                                }
-                            });
-                        }
+                        displaySpeechRecognizer();
                     }
                 });
             }
         });
+    }
+
+    private void fireMessage(final String spokenText) {
+        // Send the RPC
+        PendingResult<NodeApi.GetConnectedNodesResult> nodes = Wearable.NodeApi.getConnectedNodes(googleApiClient);
+        nodes.setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+            @Override
+            public void onResult(NodeApi.GetConnectedNodesResult result) {
+                for (int i = 0; i < result.getNodes().size(); i++) {
+                    Node node = result.getNodes().get(i);
+                    String nName = node.getDisplayName();
+                    String nId = node.getId();
+                    Log.d(TAG, "Node name and ID: " + nName + " | " + nId);
+
+                    Wearable.MessageApi.addListener(googleApiClient, new MessageApi.MessageListener() {
+                        @Override
+                        public void onMessageReceived(MessageEvent messageEvent) {
+                            Log.d(TAG, "Message received: " + messageEvent);
+                        }
+                    });
+
+                    PendingResult<MessageApi.SendMessageResult> messageResult = Wearable.MessageApi.sendMessage(googleApiClient, node.getId(),
+                            PATH, spokenText.getBytes());
+                    messageResult.setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
+                        @Override
+                        public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                            Status status = sendMessageResult.getStatus();
+                            Log.d(TAG, "Status: " + status.toString());
+                            if (status.getStatusCode() != WearableStatusCodes.SUCCESS) {
+                                //alertButton.setProgress(-1);
+                                //label.setText("Tap to retry. Alert not sent :(");
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // Do something with spokenText
+            fireMessage(spokenText);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
